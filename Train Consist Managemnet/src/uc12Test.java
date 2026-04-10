ee5e6f7efd45e47be656b6e077fe0744c52ea67b
Admin <aditi.verma2907@gmail.com>
2026-04-10 14:35:24 +0530

    uc12

---
+++ Train Consist Managemnet/src/uc12Test.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrainSafetyTest {
+
+    @Test
+    void testSafety_AllBogiesValid() {
+        List<GoodsBogie> list = Arrays.asList(
+                new GoodsBogie("Cylindrical", "Petroleum"),
+                new GoodsBogie("Open", "Coal"),
+                new GoodsBogie("Box", "Grain")
+        );
+
+        assertTrue(TrainSafetyApp.isTrainSafe(list));
+    }
+
+    @Test
+    void testSafety_CylindricalWithInvalidCargo() {
+        List<GoodsBogie> list = Arrays.asList(
+                new GoodsBogie("Cylindrical", "Coal") // invalid
+        );
+
+        assertFalse(TrainSafetyApp.isTrainSafe(list));
+    }
+
+    @Test
+    void testSafety_NonCylindricalBogiesAllowed() {
+        List<GoodsBogie> list = Arrays.asList(
+                new GoodsBogie("Open", "Coal"),
+                new GoodsBogie("Box", "Grain")
+        );
+
+        assertTrue(TrainSafetyApp.isTrainSafe(list));
+    }
+
+    @Test
+    void testSafety_MixedBogiesWithViolation() {
+        List<GoodsBogie> list = Arrays.asList(
+                new GoodsBogie("Cylindrical", "Petroleum"),
+                new GoodsBogie("Cylindrical", "Coal"), // violation
+                new GoodsBogie("Open", "Grain")
+        );
+
+        assertFalse(TrainSafetyApp.isTrainSafe(list));
+    }
+
+    @Test
+    void testSafety_EmptyBogieList() {
+        List<GoodsBogie> list = new ArrayList<>();
+
+        assertTrue(TrainSafetyApp.isTrainSafe(list));
+    }
+}
