c7ca0e2bf6ff2899ed996c9bb53a11b25d297454
Admin <aditi.verma2907@gmail.com>
2026-04-10 14:47:43 +0530

    uc14

---
+++ Train Consist Managemnet/src/PassengerBogieTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrainConsistAppTest {
+
+    @Test
+    void testException_ValidCapacityCreation() {
+        assertDoesNotThrow(() -> {
+            PassengerBogie bogie = new PassengerBogie("Sleeper", 70);
+        });
+    }
+
+    @Test
+    void testException_NegativeCapacityThrowsException() {
+        Exception exception = assertThrows(InvalidCapacityException.class, () -> {
+            new PassengerBogie("Sleeper", -10);
+        });
+
+        assertEquals("Capacity must be greater than zero", exception.getMessage());
+    }
+
+    @Test
+    void testException_ZeroCapacityThrowsException() {
+        Exception exception = assertThrows(InvalidCapacityException.class, () -> {
+            new PassengerBogie("AC Chair", 0);
+        });
+
+        assertEquals("Capacity must be greater than zero", exception.getMessage());
+    }
+
+    @Test
+    void testException_ExceptionMessageValidation() {
+        Exception exception = assertThrows(InvalidCapacityException.class, () -> {
+            new PassengerBogie("First Class", -5);
+        });
+
+        assertEquals("Capacity must be greater than zero", exception.getMessage());
+    }
+
+    @Test
+    void testException_ObjectIntegrityAfterCreation() throws InvalidCapacityException {
+        PassengerBogie bogie = new PassengerBogie("Sleeper", 80);
+
+        assertEquals("Sleeper", bogie.getType());
+        assertEquals(80, bogie.getCapacity());
+    }
+
+    @Test
+    void testException_MultipleValidBogiesCreation() {
+        assertDoesNotThrow(() -> {
+            PassengerBogie b1 = new PassengerBogie("Sleeper", 70);
+            PassengerBogie b2 = new PassengerBogie("AC Chair", 65);
+            PassengerBogie b3 = new PassengerBogie("First Class", 50);
+        });
+    }
+}
